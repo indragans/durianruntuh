619101eb9a48919362c63164e4b7af5d78d2cd9d
hafidosudirto <mochamadhafidostudent@gmail.com>
2025-09-22 19:10:19 +0700

    Update Asset
    Update BG
    Update Durian

--- Assets/Script/CorrectObject.cs
+++ Assets/Script/CorrectObject.cs
@@ -1,30 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CorrectObject : MonoBehaviour
 {
     public GameManager GM;
-    Transform tr;
+    public int points = 10;   // Normal=10, Emas=50
+    private Transform tr;
+
     void Start()
     {
-        tr = GetComponent<Transform>();
+        tr = transform;
         GM = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     void FixedUpdate()
     {
         tr.position -= new Vector3(0f, 3f * Time.fixedDeltaTime, 0f);
-        if (tr.position.y < -7f) Destroy(this.gameObject);
-
+        if (tr.position.y < -7f) Destroy(gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Basket")
+        if (collision.CompareTag("Basket"))
         {
-            Destroy(this.gameObject);
-            GM.ScoreAdd();
+            Destroy(gameObject);
+            GM.ScoreAdd(points);
         }
     }
 }
